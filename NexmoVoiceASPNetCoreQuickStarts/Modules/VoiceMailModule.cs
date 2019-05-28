90af95bbf39d56038c15b83a3be1505c1323463f
Rabeb Othmani <rabeb.othmani@vonage.com>
2019-05-28 22:21:31 +0100

    Voice mail code snippet

---
+++ NexmoVoiceASPNetCoreQuickStarts/Modules/VoiceMailModule.cs
@@ -0,0 +1,43 @@
+﻿using Nancy;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NexmoVoiceASPNetCoreQuickStarts.Modules
+{
+    public class VoiceMailModule : NancyModule
+    {
+        public VoiceMailModule()
+        {
+            Get["/webhook/answer/"] = x => { var response = (Response)GetVoiceMailNCCO();
+                                             response.ContentType = "application/json";
+                                             return response;
+                                           }; 
+            Post["/webhook/event"] = x => Request.Query["status"];
+        }
+        private dynamic GetVoiceMailNCCO()
+        {
+            dynamic TalkNCCO = new JObject();
+            TalkNCCO.action = "talk";
+            TalkNCCO.text = "Hello. You have reached Bibi. Please, leave your message after the beep.";
+            TalkNCCO.voiceName = "Emma";
+
+            dynamic RecordNCCO = new JObject();
+            RecordNCCO.action = "record";
+            RecordNCCO.beepStart = true;
+            RecordNCCO.eventUrl = "https://example.com/recording";
+            RecordNCCO.endOnSilence = 3;
+
+
+            JArray nccoObj = new JArray
+            {
+                TalkNCCO,
+                RecordNCCO
+            };
+
+            return nccoObj.ToString();
+        }
+    }
+}
